4d89e35881e99c81784bb4a2b60ef2ad52c8e3dd
alexandruakkol <alexandru.akkol@gmail.com>
2022-12-26 19:26:43 +0300

    selectionSort

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 void printArray(const int arr[], short size){
     for(char i=0; i<size; i++){
-        printf("%i\n", arr[i]);
+        printf("arr[%i]:%i\n", i, arr[i]);
     }
 }
 
@@ -14,9 +14,18 @@ void printDecimal(int i){
     printf("%i\n", i);
 }
 
-int indexOf(const char str[], short index, short size){
+int indexOfStr(const char str[], char find, short size){
     int result;
-    for(short i=0; i<size && str[i] != index; i++, result = i);
+    for(short i=0; i<size && str[i] != find; i++, result = i);
+    return result;
+}
+
+int indexOfArr(const int str[], int find, int size){
+    int result;
+    for(short i=0; i<size; i++){
+        result = i;
+        if(str[i] == find) break;
+    }
     return result;
 }
 
@@ -32,6 +41,18 @@ int min(const int arr[], short size){
     int result=arr[0];
     for(int i=0; i<size-1; i++, arr[i]<result?result=arr[i]:result);
     return result;
+}
+
+void selectionSort(int arr[], int size){ //'sideeffects' the array
+    size--;
+    while(size>=0){
+        int mx = max(arr, size+1);
+        int indexMax = indexOfArr(arr, mx, size+1);
+        int buffer = arr[size];
+        arr[size] = arr[indexMax];
+        arr[indexMax] = buffer;
+        size--;
+    } 
 }
 
 int main(){
